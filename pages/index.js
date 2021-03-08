c5bcc1492f4680e6eb7d5c75dd3a4d4fc5c1b7c4
Justin Noel <justin.noel@calendee.com>
2021-03-08 14:38:59 -0600

    feat: add twind, meta, robots

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Head from "flareact/head";
+
 export default function Index() {
   return (
-    <h1>
-      You're running React on the Edge!
-    </h1>
+    <>
+      <Head>
+        <title>Flareact Demo</title>
+        <meta
+          name="description"
+          content="Demo project for ReactJS Meetup. Shows using Flareact to create a blog powered by the Ghost API."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <h1 className="text-5xl">You're running React on the Edge!</h1>
+    </>
   );
 }
