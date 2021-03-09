a01624ba7866b32180ba0e0b92b17bf659597b4c
Justin Noel <justin.noel@calendee.com>
2021-03-09 17:15:43 -0600

    chore: prettified code

--- index.js
+++ index.js
@@ -10,18 +10,22 @@ import { handleEvent } from "flareact";
 const DEBUG = false;
 
 addEventListener("fetch", (event) => {
-  try {
-    event.respondWith(
-      handleEvent(event, require.context("./pages/", true, /\.(js|jsx|ts|tsx)$/), DEBUG)
-    );
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        })
-      );
-    }
-    event.respondWith(new Response("Internal Error", { status: 500 }));
-  }
+	try {
+		event.respondWith(
+			handleEvent(
+				event,
+				require.context("./pages/", true, /\.(js|jsx|ts|tsx)$/),
+				DEBUG,
+			),
+		);
+	} catch (e) {
+		if (DEBUG) {
+			return event.respondWith(
+				new Response(e.message || e.toString(), {
+					status: 500,
+				}),
+			);
+		}
+		event.respondWith(new Response("Internal Error", { status: 500 }));
+	}
 });
--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,26 @@
 import Document, { Html, Head, Main, FlareactScript } from "flareact/document";
 
 class MyDocument extends Document {
-  static async getEdgeProps(ctx) {
-    const props = await Document.getEdgeProps(ctx);
-    return { ...props };
-  }
+	static async getEdgeProps(ctx) {
+		const props = await Document.getEdgeProps(ctx);
+		return { ...props };
+	}
 
-  render() {
-    return (
-      <Html hidden lang="en">
-        <Head />
-        <body className="p-4">
-          <Main />
-          <FlareactScript />
-          <script
-            type="module"
-            src="https://cdn.skypack.dev/twind/shim"
-          ></script>
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html hidden lang="en">
+				<Head />
+				<body>
+					<Main />
+					<FlareactScript />
+					<script
+						type="module"
+						src="https://cdn.skypack.dev/twind/shim"
+					></script>
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
